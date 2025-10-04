9312ae448c09135648dce058f03f41f8c089143a
Melvud <ivsilan2005@gmail.com>
2025-10-04 12:27:11 +0500

    fix: Upgrade to React 19 for Payload 3.0 compatibility

--- server/next.config.mjs
+++ server/next.config.mjs
@@ -2,9 +2,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   experimental: {
-    reactCompiler: true, // Включаем для React 19
+    reactCompiler: false,
   },
 }
 
